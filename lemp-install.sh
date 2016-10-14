ad929355c72e9953d3c94b2107698e0324b84b17
Vladimir Vasic <vladimir@vasic.us>
2016-10-14 18:54:40 +0200

    Update lemp-install.sh

--- lemp-install.sh
+++ lemp-install.sh
@@ -36,6 +36,8 @@ useradd -r -s /usr/sbin/nologin nginx && mkdir -p /var/log/nginx /var/cache/ngin
 		--http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp  \
 		--http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp  \
 		--http-scgi-temp-path=/var/cache/nginx/scgi_temp  \
+		--user=www-data  \
+		--group=www-data  \
 		--with-http_ssl_module  \
 		--with-http_realip_module  \
 		--with-http_addition_module  \
@@ -87,6 +89,14 @@ tar xzvf redis-stable.tar.gz
 make
 make test
 make install
+
+# Install WordPress
+cd /tmp
+curl -O https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+cp /tmp/wordpress/wp-config-sample.php /tmp/wordpress/wp-config.php
+mkdir /tmp/wordpress/wp-content/upgrade
+cp -a /tmp/wordpress/. /etc/nginx/html
 
 apt-get purge -yqq automake autoconf libtool git-core build-essential tcl zlib1g-dev libpcre3-dev libxslt1-dev libxml2-dev libgd2-xpm-dev libgeoip-dev libgoogle-perftools-dev libperl-dev &&
 apt-get autoremove -yqq &&
